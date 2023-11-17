05a70fdbb1030b9902f993eb4611960b6537c035
milosz <mdubielel@gmail.com>
2023-11-17 21:51:33 +0100

    Delete app template and install design and navigation dependencies

--- babel.config.js
+++ babel.config.js
@@ -10,4 +10,9 @@ module.exports = {
     ],
   ],
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
