a28061388966ad7ff1b7412b0a8623dcd8dab556
milosz <mdubielel@gmail.com>
2024-01-06 00:35:18 +0100

    Check if device is connected to INet and display waiting modal for configuring device

--- src/pages/NewDevicePage.tsx
+++ src/pages/NewDevicePage.tsx
@@ -13,23 +13,30 @@ import {
   IonSelect,
   IonSelectOption,
   IonSpinner,
+  IonText,
+  useIonAlert,
 } from '@ionic/react';
-import { useEffect, useRef, useState } from 'react';
+import { CSSProperties, useEffect, useRef, useState } from 'react';
 import { z } from 'zod';
 import { ScanButton } from '../components/wifi/ScanButton';
 import { AvailableWifiList } from '../components/wifi/AvailableWifiList';
 import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitButton } from '../components/forms/SubmitButton';
-import { WifiSecurityProtocol } from '../utils/WifiSecurityProtocol';
+import {
+  WifiSecurityProtocol,
+  WifiSecurityProtocolMapForWifiWizard,
+} from '../utils/WifiSecurityProtocol';
 import { checkmarkOutline } from 'ionicons/icons';
 import { IonLabelRight } from '../components/helpers/IonLabelRight';
+import { WifiWizard2 } from '@awesome-cordova-plugins/wifi-wizard-2';
+import { useHistory } from 'react-router';
 
 const wifiDataValidationSchema = z
   .object({
     SSID: z.string().min(1),
     BSSID: z.string(),
-    algorithm: z.string(),
+    algorithm: z.nativeEnum(WifiSecurityProtocol),
     password: z.string().optional(),
   })
   .refine(
@@ -57,7 +64,42 @@ export type FoundWifiDetails = {
 };
 
 export const NewDevicePage = () => {
-  const modal = useRef<HTMLIonModalElement>(null);
+  const [currentINetConnSSID, setCurrentINetConnSSID] = useState<
+    string | undefined
+  >(undefined);
+
+  // TODO (FUTURE) integrate presenting alert and awaiting
+  // for user interaction with button to execute retry
+  const [presentAlert, dismissAlert] = useIonAlert();
+  const history = useHistory();
+  useEffect(() => {
+    if (currentINetConnSSID) return;
+
+    WifiWizard2.getConnectedSSID()
+      .then(ssid => {
+        // IDK if this changes anything, I haven't tested this case
+        WifiWizard2.canConnectToInternet().then(() =>
+          setCurrentINetConnSSID(ssid),
+        );
+      })
+      .catch(() => {
+        dismissAlert().then(() => {
+          console.debug('present');
+          presentAlert({
+            message: `You are not connected to the Internet! Connect and try again.`,
+            buttons: [
+              {
+                text: 'OK',
+                handler: () => history.goBack(), // TODO prevent android hardware button from going back
+              },
+            ],
+            backdropDismiss: false,
+          });
+        });
+      });
+  }, [currentINetConnSSID, dismissAlert, presentAlert, history]);
+
+  const getWifiConfigurationModal = useRef<HTMLIonModalElement>(null);
   const [isOpen, setIsOpen] = useState(false);
 
   const [isLoadingWifis, setIsLoadingWifis] = useState<boolean | null>(null);
@@ -69,7 +111,7 @@ export const NewDevicePage = () => {
     password: '',
   });
 
-  const [enabledAPN, setEnabledAPN] = useState(false);
+  const [enabledAPN, setEnabledAPN] = useState<boolean>(false);
 
   const form = useForm<WifiDataForm>({
     mode: 'onChange',
@@ -82,16 +124,35 @@ export const NewDevicePage = () => {
   const { formState, control, setValue, watch, handleSubmit } = form;
   const algorithm = watch('algorithm');
 
-  // DEBUG CODE
+  const [isConfiguringDeviceCompleted, setIsConfiguringDeviceCompleted] =
+    useState<boolean>(false);
+  const waitToConfigureDeviceModal = useRef<HTMLIonModalElement>(null);
+
   useEffect(() => {
-    const subscription = watch((value, { name, type }) => {
-      console.log(value, name, type),
-        console.log(wifiDataValidationSchema.safeParse(value));
-    });
-    console.debug(formState);
-    return () => subscription.unsubscribe();
-  }, [watch, formState]);
-  // DEBUG CODE END
+    if (!enabledAPN) return;
+
+    // Connect to device's APN -- hardcoded configuration
+    // TODO for mock purposes we connect to provided WIFI configuration
+    WifiWizard2.connect(
+      selectedWifi.SSID,
+      true,
+      selectedWifi.password,
+      WifiSecurityProtocolMapForWifiWizard[selectedWifi.algorithm],
+    )
+      .then(v => console.debug(v))
+      .catch(e => console.error(e));
+
+    // Send POST request with provided WIFI configuration to device
+    // and wait for device's response that request was successful
+    // and for device's data
+
+    // Switch from device's APN to 'main' network
+
+    // Send POST request to Azure with device's data
+
+    // TODO decide with team what is our approach in communicating
+    // between Azure and device while configuration
+  }, [enabledAPN, selectedWifi]);
 
   return (
     <IonPage>
@@ -169,7 +230,7 @@ export const NewDevicePage = () => {
         </IonList>
 
         <IonModal
-          ref={modal}
+          ref={getWifiConfigurationModal}
           isOpen={isOpen}
           canDismiss={async (_, role) => role !== 'gesture'}
           initialBreakpoint={1}
@@ -263,7 +324,43 @@ export const NewDevicePage = () => {
             <SubmitButton formState={formState}>Continue</SubmitButton>
           </form>
         </IonModal>
+
+        <IonModal
+          ref={waitToConfigureDeviceModal}
+          isOpen={enabledAPN && !isConfiguringDeviceCompleted}
+          canDismiss={async (_, role) => role !== 'gesture'}
+          backdropDismiss={false}
+          initialBreakpoint={1}
+          breakpoints={[0, 1]}>
+          <div style={styles.waitToConfigureDeviceModalContent}>
+            <IonText
+              color="dark"
+              style={styles.waitToConfigureDeviceModalContentText}>
+              Please wait
+            </IonText>
+            <IonSpinner />
+            <IonText
+              color="medium"
+              style={styles.waitToConfigureDeviceModalContentText}>
+              Device is being configured.
+            </IonText>
+          </div>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
 };
+
+const styles = {
+  waitToConfigureDeviceModalContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '200px',
+  } as const,
+  waitToConfigureDeviceModalContentText: {
+    paddingTop: '10px',
+    paddingBottom: '10px',
+  } as const,
+} satisfies Record<string, CSSProperties>;
--- src/utils/WifiSecurityProtocol.ts
+++ src/utils/WifiSecurityProtocol.ts
@@ -8,3 +8,16 @@ export const WifiSecurityProtocol = {
   WPA2_ENTERPRISE: 'WPA2 Enterprise',
   WPA3_ENTERPRISE: 'WPA3 Enterprise',
 } as const;
+
+// Values supported by WifiWizard:
+// WPA, WPA (for WPA2), WEP or NONE
+export const WifiSecurityProtocolMapForWifiWizard = {
+  None: 'NONE',
+  WEP: 'WEP',
+  WPA: 'WPA',
+  'WPA2/WPA3': 'WPA',
+  WPA3: 'WPA',
+  'WPA Enterprise': 'WPA',
+  'WPA2 Enterprise': 'WPA',
+  'WPA3 Enterprise': 'WPA',
+} as const;
