38fe874f051aee5d820a446e6b8cb6f8bc51653b
milosz <mdubielel@gmail.com>
2024-01-19 22:00:04 +0100

    Add device data request

--- src/pages/DeviceDetailsPage.tsx
+++ src/pages/DeviceDetailsPage.tsx
@@ -1,8 +1,33 @@
 import { IonContent, IonPage, IonText } from '@ionic/react';
+import { useContext } from 'react';
 import { useParams } from 'react-router';
+import useSWR from 'swr';
+import UserContext from '../contexts/UserContext';
+import { Spinner } from '../components/Spinner';
+
+type DeviceDetails = {
+  id: string;
+  name: string;
+  value: number;
+};
 
 export const DeviceDetailsPage = () => {
   const { id } = useParams<{ id: string }>();
+
+  const user = useContext(UserContext);
+  const { data: deviceDetails, error } = useSWR<DeviceDetails, Error>(
+    `https://iot-project-agh-bcdgl.azurewebsites.net/api/data/${id}`,
+    (url: string) =>
+      fetch(url, {
+        headers: {
+          Authorization: `Bearer ${user?.user?.authentication.accessToken}`,
+          'x-functions-key':
+            'iNjJu8MziIYZumeq3ZUY1Wc4xvBcD240Kj7xrXt0qcvQAzFudlnkyw==',
+        },
+      }).then(res => res.json()),
+  );
+
+  if (!deviceDetails) return <Spinner />;
   return (
     <IonPage>
       {/* <IonHeader>
@@ -11,7 +36,9 @@ export const DeviceDetailsPage = () => {
         </IonToolbar>
       </IonHeader> */}
       <IonContent fullscreen>
-        <IonText>DeviceDetailsPage for: {id}</IonText>
+        <IonText>
+          DeviceDetailsPage for: {id} {deviceDetails.name}
+        </IonText>
       </IonContent>
     </IonPage>
   );
